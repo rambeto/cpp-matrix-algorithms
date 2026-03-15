97c3f29fa35a6e9684efd99a5c72d64ac53b41ee
Ramadan Eid <138159929+rambeto@users.noreply.github.com>
2026-03-15 15:47:23 +0200

    Update 18_Matrix-Intersected-Numbers.cpp

--- 18_Matrix-Intersected-Numbers.cpp
+++ 18_Matrix-Intersected-Numbers.cpp
@@ -1,11 +1,11 @@
 /*
-Problem #17
-Check if a specific number exists in a matrix.
+Problem #18
+Print intersected numbers between two matrices.
 
 Concepts:
 - 2D Arrays
-- Linear Search in Matrices
 - Nested Loops
+- Searching Algorithms (Intersection)
 */
 
 #include <iostream>
@@ -15,60 +15,47 @@ Concepts:
 
 using namespace std;
 
-/*
------------------------------------------------------------
-Function: IsExistsNumberInMatrix
-Purpose : Search for a specific number in the matrix.
------------------------------------------------------------
-*/
-bool IsExistsNumberInMatrix(int arr[3][3], int Number, short Rows, short Cols)
-{
-    for (short i = 0; i < Rows; i++)
-    {
-        for (short j = 0; j < Cols; j++)
-        {
-            if (arr[i][j] == Number)
-                return true; // Found the number
-        }
-    }
-    return false; // Number not found
-}
+// ... (الدوال RandomNumber, FillMatrixWithRandomNumbers, PrintMatrix, IsExistsNumberInMatrix كما هي في كودك) ...
 
 /*
 -----------------------------------------------------------
-Function: PrintMatrix
-Purpose : Display the matrix elements.
+Function: PrintIntersectedNumbers
+Purpose : Print common elements found in both matrices.
 -----------------------------------------------------------
 */
-void PrintMatrix(int arr[3][3], short Rows, short Cols)
+void PrintIntersectedNumbers(int arr1[3][3], int arr2[3][3], short Rows, short Cols)
 {
+    cout << "\nIntersected Numbers:\n";
     for (short i = 0; i < Rows; i++)
     {
         for (short j = 0; j < Cols; j++)
         {
-            printf(" %0*d  ", 2, arr[i][j]);
+            if (IsExistsNumberInMatrix(arr2, arr1[i][j], Rows, Cols))
+            {
+                cout << arr1[i][j] << "\t";
+            }
         }
-        cout << "\n";
     }
+    cout << endl;
 }
 
 int main()
 {
     srand((unsigned)time(NULL));
 
-    int arr[3][3];
+    int arr1[3][3], arr2[3][3];
     short Rows = 3, Cols = 3;
-    int NumberToSearch = 4;
 
-    FillMatrixWithRandomNumbers(arr, Rows, Cols);
+    FillMatrixWithRandomNumbers(arr1, Rows, Cols);
+    FillMatrixWithRandomNumbers(arr2, Rows, Cols);
 
-    cout << "\nGenerated Matrix:\n";
-    PrintMatrix(arr, Rows, Cols);
+    cout << "\nMatrix [1]:\n";
+    PrintMatrix(arr1, Rows, Cols);
     
-    if (IsExistsNumberInMatrix(arr, NumberToSearch, Rows, Cols))
-        cout << "\nYes, the number " << NumberToSearch << " exists in the matrix.\n";
-    else
-        cout << "\nNo, the number " << NumberToSearch << " does not exist in the matrix.\n";
+    cout << "\nMatrix [2]:\n";
+    PrintMatrix(arr2, Rows, Cols);
+    
+    PrintIntersectedNumbers(arr1, arr2, Rows, Cols);
 
     return 0;
 }
